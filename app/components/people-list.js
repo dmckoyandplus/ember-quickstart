de718ea5cde4c5402020d78552fe51a322ba5fef
dmckoyandplus <dmckoy@andplus.com>
2019-01-19 20:34:53 -0500

    added example action to list items

--- app/components/people-list.js
+++ app/components/people-list.js
@@ -1,4 +1,15 @@
 import Component from '@ember/component';
 
 export default Component.extend({
+  actions: {
+    showPerson(person) {
+      switch(person){
+        case 'Crash Override':
+          alert('Mess with the best, die like the rest.');
+          break;
+        default:
+          alert(`${person} from that horrible 80's movie`);
+      }
+    }
+  }
 });
